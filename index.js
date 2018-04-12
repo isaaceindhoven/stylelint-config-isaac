7cc2bec9cb490c2599bed78b74b8468b2a9204bc
Ferry Brouwer <ferry.brouwer@isaac.nl>
2018-04-12 16:51:08 +0200

    [TASK] Add extended rules

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: 'stylelint-config-sass-guidelines'
+  extends: 'stylelint-config-sass-guidelines',
+  rules: {
+    'indentation': 4,
+    'order/properties-alphabetical-order': false,
+    'rule-empty-line-before': 'never-multi-line',
+    'max-nesting-depth': [3, {ignoreAtRules: ['media', 'supports', 'include']}],
+    'selector-no-qualifying-type': [true, {ignore: ['attribute', 'class']}],
+  },
 };
