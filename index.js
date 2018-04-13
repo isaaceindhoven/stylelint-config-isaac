b76ab4979a0da2a53ca0fb6caf03bb6363df4ffe
Ferry Brouwer <ferry.brouwer@isaac.nl>
2018-04-13 10:21:36 +0200

    [FIX] to disable alphabetical order we now use value `null` instead `false`

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'stylelint-config-sass-guidelines',
   rules: {
     'indentation': 4,
-    'order/properties-alphabetical-order': false,
+    'order/properties-alphabetical-order': null,
     'rule-empty-line-before': 'never-multi-line',
     'max-nesting-depth': [3, {ignoreAtRules: ['media', 'supports', 'include']}],
     'selector-no-qualifying-type': [true, {ignore: ['attribute', 'class']}],
